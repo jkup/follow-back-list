40c12ea583f3a7ac12f2fadee3929c21f48ef399
Jon Kuperman <jon.kuperman@gmail.com>
2015-12-06 17:03:44 -0800

    initial setup

---
+++ config/settings.js
@@ -0,0 +1,11 @@
+const Twitter = require('twitter');
+
+module.exports = function() {
+  return Twitter({
+    consumer_key: '',
+    consumer_secret: '',
+    access_token_key: '',
+    access_token_secret: ''
+  });
+
+}
---
+++ index.js
@@ -0,0 +1,10 @@
+const Twitter = require('twitter');
+
+const client = require('./config/settings');
+
+const params = {screen_name: 'nodejs'};
+client.get('statuses/user_timeline', params, function(error, tweets, response){
+  if (!error) {
+    console.log(tweets);
+  }
+});
