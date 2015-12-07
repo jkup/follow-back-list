55a4e426078d53ee66822b9765b7fb0935d7d1ca
Jon Kuperman <jon.kuperman@gmail.com>
2015-12-06 22:15:02 -0800

    only grab the first 100

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ var usersFinal = [];
 
 // First get your followers
 client.get('followers/ids', { screen_name: screen_name }, function(error, your_followers, response){
-  if(error) throw error;
+  if (error) { console.log(error); }
 
   var followers = your_followers.ids;
 
   // Then get your following
   client.get('friends/ids', { screen_name: screen_name }, function(error, your_following, response){
-    if(error) throw error;
+    if (error) { console.log(error); }
 
     var following = your_following.ids;
 
@@ -24,12 +24,15 @@ client.get('followers/ids', { screen_name: screen_name }, function(error, your_f
       }
     });
 
+    // First get only first 100 results
+    one_way_following = one_way_following.slice(0, 99);
+
     // Turn user ids into a string
     var user_id_string = one_way_following.join();
 
     // Then get the people
     client.get('users/lookup', { user_id: user_id_string }, function(error, users, response) {
-      if(error) throw error;
+      if (error) { console.log(error); }
 
       users.forEach(function(user) {
         var userObject = {
