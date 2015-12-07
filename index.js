69916f718d1fde1cf338e9256e2e16b82e32124e
Jon Kuperman <jon.kuperman@gmail.com>
2015-12-06 23:19:47 -0800

    add some basic styling

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var userList   = require('./user-list');
 var app        = express();
 
 app.set('view engine', 'jade');
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(sanitize());
 
