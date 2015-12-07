f0ce73d0197376000bb21dec211898db55b5dbc8
Jon Kuperman <jon.kuperman@gmail.com>
2015-12-06 22:26:06 -0800

    first attempt at express

--- index.js
+++ index.js
@@ -1,50 +1,20 @@
-const Twitter = require('twitter');
-const client  = require('./config/settings');
+var express  = require('express');
+var userList = require('./user-list');
+var app      = express();
 
-const screen_name = 'kng';
-var one_way_following = [];
-var usersFinal = [];
+var screen_name       = 'kng';
 
-// First get your followers
-client.get('followers/ids', { screen_name: screen_name }, function(error, your_followers, response){
-  if (error) { console.log(error); }
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
 
-  var followers = your_followers.ids;
+app.get('/list/:screen_name', function (req, res) {
+  const screen_name = params.get.screen_name;
+  userList(screen_name);
+});
 
-  // Then get your following
-  client.get('friends/ids', { screen_name: screen_name }, function(error, your_following, response){
-    if (error) { console.log(error); }
+var server = app.listen(3000, function () {
+  var port = server.address().port;
 
-    var following = your_following.ids;
-
-    // Iterate through each follower
-    following.forEach(function(follow) {
-      if (followers.indexOf(follow) === -1) {
-        one_way_following.push(follow);
-      }
-    });
-
-    // First get only first 100 results
-    one_way_following = one_way_following.slice(0, 99);
-
-    // Turn user ids into a string
-    var user_id_string = one_way_following.join();
-
-    // Then get the people
-    client.get('users/lookup', { user_id: user_id_string }, function(error, users, response) {
-      if (error) { console.log(error); }
-
-      users.forEach(function(user) {
-        var userObject = {
-          name: user.name,
-          screen_name: user.screen_name,
-          avatar: user.profile_image_url
-        };
-
-        usersFinal.push(userObject);
-      });
-
-      console.log(usersFinal);
-    });
-  });
+  console.log('Example app listening at http://localhost:%s', port);
 });
---
+++ user-list.js
@@ -0,0 +1,51 @@
+var Twitter  = require('twitter');
+var client   = require('./config/settings');
+
+module.exports = function(screen_name) {
+  var one_way_following = [];
+  var usersFinal        = [];
+
+  // First get your followers
+  client.get('followers/ids', { screen_name: screen_name }, function(error, your_followers, response){
+    if (error) { console.log(error); }
+
+    var followers = your_followers.ids;
+
+    // Then get your following
+    client.get('friends/ids', { screen_name: screen_name }, function(error, your_following, response){
+      if (error) { console.log(error); }
+
+      var following = your_following.ids;
+
+      // Iterate through each follower
+      following.forEach(function(follow) {
+        if (followers.indexOf(follow) === -1) {
+          one_way_following.push(follow);
+        }
+      });
+
+      // First get only first 100 results
+      one_way_following = one_way_following.slice(0, 99);
+
+      // Turn user ids into a string
+      var user_id_string = one_way_following.join();
+
+      // Then get the people
+      client.get('users/lookup', { user_id: user_id_string }, function(error, users, response) {
+        if (error) { console.log(error); }
+
+        users.forEach(function(user) {
+          var userObject = {
+            name: user.name,
+            screen_name: user.screen_name,
+            avatar: user.profile_image_url
+          };
+
+          usersFinal.push(userObject);
+        });
+
+        console.log(usersFinal);
+      });
+    });
+  });
+}
